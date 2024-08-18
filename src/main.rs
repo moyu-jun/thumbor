2a640b3e683f5cec20eabd042ed8e63a88ab1234
moyu <moyubit@gmail.com>
2024-08-18 14:57:15 +0800

    初始化项目

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+// struct ImageSpec {
+//     specs: Vec<Spec>,
+// }
+
+// enum Spec {
+//     Resize(Resize),
+//     Crop(Crop),
+// }
+
+// struct Resize {
+//     width: u32,
+//     height: u32,
+// }
+
+// struct Crop {
+//     x: u32,
+//     y: u32,
+//     width: u32,
+//     height: u32,
+// }
+
+fn main() {
+    println!("Hello, world!");
+}
